900e0d01e0566c24064257f1e7995634e52079b2
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2019-06-18 23:27:17 -0500

    Hola Mundo

---
+++ hola_mundo.php
@@ -0,0 +1,21 @@
+<?php
+
+//echo nos sirve para mostrar información en pantalla
+#Otro comentario
+/*
+Otro comentario más
+*/
+#echo "Hola mundo!";
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1><?php echo "Hola mundo";?></h1>
+</body>
+</html>
