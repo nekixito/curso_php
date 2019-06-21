df0be50218c68f82e1b2c4d506281ba493c13c68
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2019-06-21 17:10:44 -0500

    Contar elementos de un arreglo

---
+++ contar_elementos_arreglo.php
@@ -0,0 +1,15 @@
+<?php 
+
+$meses = array('Enero','Febrero','Marzo','Abril',
+				'Mayo','Junio','Julio','Agosto','Septiembre',
+				'Octubre','Noviembre','Diciembre');
+
+echo count($meses).'<br />';
+
+$ultimo_mes = count($meses)-1;
+
+echo $meses[$ultimo_mes];
+
+
+
+ ?>
