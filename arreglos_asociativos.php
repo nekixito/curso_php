5e1f1a73dd832a23f68c92a5ad2a6c42696efd8e
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2019-06-20 21:39:48 -0500

    Creación de arreglo asociativo

---
+++ arreglos_asociativos.php
@@ -0,0 +1,12 @@
+<?php 
+
+$miguel = array('telefono'=>'5566778899','edad'=>33,'apellido'=>'meza','pais'=>'mexico');
+
+$miguel['telefono']='5511223344';
+
+echo 'El teléfono de Miguel es: '.$miguel['telefono'].'<br />';
+echo $miguel['edad'].'<br />';
+echo $miguel['apellido'].'<br />';
+echo $miguel['pais'].'<br />';
+
+ ?>
