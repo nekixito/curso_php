b43217ba0e1768b6585302dedb04bd15aab2eb31
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2019-07-02 18:48:51 -0500

    Else

---
+++ else.php
@@ -0,0 +1,17 @@
+<?php 
+
+$mes='Diciembre';
+
+if($mes == 'Diciembre'){
+	echo "Feliz Navidad";
+} else if($mes == 'Enero'){
+	echo "Feliz Año Nuevo";
+} else if($mes == 'Julio'){
+	echo "Feliz Julio";
+} else {
+	echo "El mes que pusiste no tiene celebración";
+}
+
+
+
+ ?>
