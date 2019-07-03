b2ab43934e2c0952e47056ec833a7d1df5178826
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2019-07-02 22:21:54 -0500

    función var_dump()

---
+++ var_dump.php
@@ -0,0 +1,35 @@
+<?php 
+
+$texto = 'Carlos';
+$numero=10;
+$numero2='5';
+$arreglo=array('Carlos','Cesar','Alejandro');
+$arreglo_asociativo=array('nombre'=>'Carlos','edad'=>20);
+$booleano=false;
+
+echo '<pre>';
+var_dump($texto);
+echo '</pre>';
+echo '<br />';
+echo '<pre>';
+var_dump($numero);
+echo '</pre>';
+echo '<br />';
+echo '<pre>';
+var_dump($numero2);
+echo '</pre>';
+echo '<br />';
+echo '<pre>';
+var_dump($arreglo);
+echo '</pre>';
+echo '<br />';
+echo '<pre>';
+var_dump($arreglo_asociativo);
+echo '</pre>';
+echo '<br />';
+echo '<pre>';
+var_dump($booleano);
+echo '</pre>';
+echo '<br />';
+
+ ?>
