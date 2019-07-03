2e766d6289fda5f9f55d881ae8e0b22408853f9f
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2019-07-03 07:55:39 -0500

    Función con parámetros

---
+++ funciones_argumentos.php
@@ -0,0 +1,25 @@
+<?php 
+
+/*
+function saludo($nombre){
+
+	echo 'Saludos '.$nombre.'!';
+	echo "<br />";
+}
+
+saludo('Miguel');
+saludo('Gabriel');
+saludo('Rafael');
+*/
+
+function sumar($numero1,$numero2){
+	$resultado=$numero1+$numero2;
+	echo $resultado;
+	echo "<br />";
+}
+
+sumar(3,5);
+sumar(43,98);
+sumar(321,19);
+
+ ?>
