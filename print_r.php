e6e09968f6ecea787f7bdd733d64194b8914db13
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2019-07-02 22:29:32 -0500

    función print_r()

---
+++ print_r.php
@@ -0,0 +1,35 @@
+<?php 
+
+$texto = 'Carlos';
+$numero=10;
+$numero2='5';
+$arreglo=array('Carlos','Cesar','Alejandro');
+$arreglo_asociativo=array('nombre'=>'Carlos','edad'=>20);
+$booleano=false;
+
+echo '<pre>';
+print_r($texto);
+echo '</pre>';
+echo '<br />';
+echo '<pre>';
+print_r($numero);
+echo '</pre>';
+echo '<br />';
+echo '<pre>';
+print_r($numero2);
+echo '</pre>';
+echo '<br />';
+echo '<pre>';
+print_r($arreglo);
+echo '</pre>';
+echo '<br />';
+echo '<pre>';
+print_r($arreglo_asociativo);
+echo '</pre>';
+echo '<br />';
+echo '<pre>';
+print_r($booleano);
+echo '</pre>';
+echo '<br />';
+
+ ?>
