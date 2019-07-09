36370df4b7e21939d096e68e3c39f1d1caddfb12
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2019-07-08 21:24:18 -0500

    Consulta

--- bd/pdo.php
+++ bd/pdo.php
@@ -1,8 +1,18 @@
 <?php 
 try{
 
+	$id = $_GET['id'];
+
+	//El usuario puede inyectar código
 	$conexion=new PDO('mysql:host=localhost;dbname=prueba_datos','root','');
-	echo "Conexión OK";
+	//echo "Conexión OK";
+
+	$resultados = $conexion->query("SELECT * FROM usuarios WHERE id=$id");
+
+	foreach ($resultados as $fila) {
+		echo $fila['nombre'].'<br>';
+	}
+
 }catch(PDOException $e){
 	echo "Error: "+$e->getMessage();
 }
