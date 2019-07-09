c6b66affecc40dd29a39fc867f8c4156d4821752
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2019-07-09 13:41:14 -0500

    Paginación

---
+++ practicas/paginacion/index.php
@@ -0,0 +1,38 @@
+<?php
+
+try{
+    $conexion = new PDO('mysql:host=localhost;dbname=paginacion','root','');
+
+
+} catch (PDOException $e){
+    echo "ERROR: ".$e->getMessage();
+    die();
+}
+
+$pagina = isset($_GET['pagina'])?(int)$_GET['pagina']:1;
+$postPorPagina = 5;
+
+$inicio = ($pagina > 1) ? ($pagina*$postPorPagina-$postPorPagina):0;
+
+$articulos = $conexion->prepare("SELECT SQL_CALC_FOUND_ROWS * FROM articulos 
+                                LIMIT $inicio, $postPorPagina
+                                ");
+
+$articulos->execute();
+$articulos = $articulos->fetchAll();
+
+//print_r($articulos);
+if(!$articulos){
+    header('Location: index.php');
+}
+
+$totalArticulos=$conexion->query('SELECT FOUND_ROWS() as total');
+$totalArticulos=$totalArticulos->fetch()['total'];
+//echo $totalArticulos;
+
+$numeroPaginas = ceil($totalArticulos/$postPorPagina);
+//echo $numeroPaginas;
+
+require 'index.view.php';
+
+?>
