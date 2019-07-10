9e1b38948c433a234fbfc98e57ed135b8705e06f
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2019-07-10 11:09:02 -0500

    Contenido login_registro

---
+++ practicas/login_registro/contenido.php
@@ -0,0 +1,9 @@
+<?php 
+
+require 'views/contenido.view.php';
+
+
+
+
+
+ ?>
---
+++ practicas/login_registro/views/contenido.view.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta class="viewport" content="width=device-width,user-scalable=no,
+	initial-scale=1.0,maximum-scale=1.0,minimum-scale:1.0">
+	<link href="https://fonts.googleapis.com/css?family=Raleway:300,400&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="css/estilos.css">
+	<title>Contenido</title>
+</head>
+<body>
+	<div class="contenedor">
+		<h1 class="titulo">Contenido del sitio</h1>
+		<a href="cerrar.php">Cerrar sesión</a>
+		
+		<div class="contenido">
+			<article>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vestibulum tincidunt lectus, ac porta metus congue volutpat. Nunc luctus lorem et sodales imperdiet. Fusce dignissim dignissim lacus at commodo. Praesent dolor eros, accumsan a molestie vitae, auctor et ipsum. Proin vehicula metus in fermentum luctus. Ut venenatis elementum mi, nec imperdiet enim malesuada at. Suspendisse potenti. Etiam auctor risus sit amet faucibus placerat. Integer id dolor non ante blandit convallis a ac nibh. Proin vulputate imperdiet massa.
+				</p>
+				<p>
+					Duis ex metus, convallis quis facilisis at, lobortis quis diam. Ut feugiat posuere odio sit amet maximus. Sed vel varius tortor. Pellentesque elit ex, cursus sed facilisis ut, hendrerit in ex. Etiam euismod tellus laoreet orci interdum auctor. Sed eleifend varius urna, a accumsan nisi porttitor quis. Nunc eget tincidunt eros, nec sollicitudin libero. In in mollis magna. Nunc sollicitudin, nibh at ultricies bibendum, justo dui dictum augue, ut aliquam sapien metus in nisi. Sed tincidunt ex et mi viverra sagittis.
+				</p>
+			</article>
+		</div>
+	</div>
+</body>
+</html>
