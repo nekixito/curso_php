60e084c8f265906caa73891098d05a8f1c7d13fd
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2019-07-11 13:05:05 -0500

    Página de inicio

---
+++ practicas/galeria/views/index.view.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link href='https://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/estilos.css">
+	<title>Galeria</title>
+</head>
+<body>
+	<header class="contenedor">
+		<h1 class="titulo">Mi increible Galería en PHP y MySQL</h1>
+	</header>
+
+	<section class="fotos">
+		<div class="contenedor">
+			<div class="thumb">
+				<a href="#">
+					<img src="imagenes/1.jpg">
+				</a>
+			</div>
+			<div class="thumb">
+				<a href="#">
+					<img src="imagenes/2.jpg">
+				</a>
+			</div>
+			<div class="thumb">
+				<a href="#">
+					<img src="imagenes/3.jpg">
+				</a>
+			</div>
+			<div class="thumb">
+				<a href="#">
+					<img src="imagenes/4.jpg">
+				</a>
+			</div>
+			<div class="thumb">
+				<a href="#">
+					<img src="imagenes/1.jpg">
+				</a>
+			</div>
+			<div class="thumb">
+				<a href="#">
+					<img src="imagenes/2.jpg">
+				</a>
+			</div>
+			<div class="thumb">
+				<a href="#">
+					<img src="imagenes/3.jpg">
+				</a>
+			</div>
+			<div class="thumb">
+				<a href="#">
+					<img src="imagenes/4.jpg">
+				</a>
+			</div>
+
+			<div class="paginacion">
+				<a href="#" class="izquierda"><i class="fa fa-long-arrow-left"></i> Página Anterior</a>
+				<a href="#" class="derecha">Página Siguiente <i class="fa fa-long-arrow-right"></i></a>
+			</div>
+		</div>		
+	</section>
+
+	<footer>
+		<p class="copyright">
+			Galería creada por Miguel.
+		</p>
+	</footer>
+
+
+</body>
+</html>
