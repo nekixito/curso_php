d89c8c3bb2dafec75fea4bbb16907ec4c4e62bfd
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2019-07-12 11:26:06 -0500

    Obtener articulos de la bd

--- practicas/blog/functions.php
+++ practicas/blog/functions.php
@@ -4,7 +4,7 @@
 
 function conexion($bd_config){
 	try {
-		$conexion = new PDO('mysql:host=localhost;dbname=blog_practica','root','');
+		$conexion = new PDO('mysql:host=localhost;dbname='.$bd_config['basedatos'],$bd_config['usuario'],$bd_config['pass']);
 		return $conexion;
 		
 	} catch (Exception $e) {
@@ -18,6 +18,23 @@ function limpiarDatos($datos){
 	$datos = htmlspecialchars($datos);
 	return $datos;
 }
+
+function pagina_actual(){
+
+	return isset($_GET['p'])?(int)$_GET['p']:1;
+}
+
+function obtener_post($post_por_pagina,$conexion){
+	$inicio = (pagina_actual()>1)? pagina_actual()*$post_por_pagina-$post_por_pagina:0;
+
+	$sentencia = $conexion->prepare("SELECT SQL_CALC_FOUND_ROWS * FROM articulos LIMIT $inicio,$post_por_pagina");
+
+	$sentencia->execute();
+	return $sentencia->fetchAll();
+
+}
+
+
 
 
 
--- practicas/blog/index.php
+++ practicas/blog/index.php
@@ -1,8 +1,26 @@
 <?php 
 
 require 'admin/config.php';
-require 'funtions.php';
+require 'functions.php';
 
+$conexion = conexion($bd_config);
+
+if(!$conexion){
+	header('Location: error.php');
+}
+
+$posts = obtener_post($blog_config['post_por_pagina'],$conexion);
+if(!$posts){
+	header('Location: error.php');
+}
+
+//echo pagina_actual();
+/*
+$statement = $conexion->prepare("SELECT SQL_CALC_FOUND_ROWS * FROM fotos LIMIT 0,2");
+
+$statement->execute();
+$fotos = $statement->fetchAll();
+*/
 
 require 'views/index.view.php';
 
