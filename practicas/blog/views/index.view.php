9448a02c211c3bc078ce1b39e3034323fd250651
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2019-07-12 11:49:06 -0500

    Cargar artículos en index

--- practicas/blog/views/index.view.php
+++ practicas/blog/views/index.view.php
@@ -1,35 +1,26 @@
 <?php require 'header.php' ?>
 
 	<div class="contenedor">
-		<div class="post">
-			<article>
-				<h2 class="titulo"><a href="#">Titulo del artículo</a></h2>
-				<p class="fecha">12 de Julio del 2019</p>
-				<div class="thumb">
-					<a href="#">
-						<img src="<?php echo RUTA; ?>/imagenes/1.png" alt="">
-					</a>
-				</div>
-				<p class="extracto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam, at.</p>
-				<a href="#" class="continuar">Continuar Leyendo</a>
-				
-			</article>
-		</div>
 
-		<div class="post">
+		<?php foreach($posts as $post): ?>
+			<div class="post">
 			<article>
-				<h2 class="titulo"><a href="#">Titulo del artículo</a></h2>
-				<p class="fecha">12 de Julio del 2019</p>
+				<h2 class="titulo"><a href="single.php?id=<?php echo $post['id']; ?>"><?php echo $post['titulo']; ?></a></h2>
+				<p class="fecha"><?php echo $post['fecha']; ?></p>
 				<div class="thumb">
-					<a href="#">
-						<img src="<?php echo RUTA; ?>/imagenes/1.png" alt="">
+					<a href="single.php?id=<?php echo $post['id']; ?>">
+						<img src="<?php echo RUTA; ?>/imagenes/<?php echo $post['thumb']; ?>" alt="">
 					</a>
 				</div>
-				<p class="extracto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam, at.</p>
-				<a href="#" class="continuar">Continuar Leyendo</a>
+				<p class="extracto"><?php echo $post['extracto']; ?></p>
+				<a href="single.php?id=<?php echo $post['id']; ?>" class="continuar">Continuar Leyendo</a>
 				
 			</article>
 		</div>
+			
+		<?php endforeach; ?>
+
+		
 
 		<?php require 'paginacion.php' ?>
 	</div>
