1b5f20608836bc35df133b42637f37decafe3518
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2019-07-12 14:14:28 -0500

    Página de error

---
+++ practicas/blog/error.php
@@ -0,0 +1,15 @@
+<?php 
+require 'admin/config.php';
+
+require 'views/header.php';
+ ?>
+
+ <div class="contenedor">
+ 	<div class="post">
+ 		<h2 class="titulo">Error</h2>
+ 		<br>
+
+ 	</div>
+ </div>
+
+ <?php require 'views/footer.php' ?>
