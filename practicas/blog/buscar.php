385db0f73458bc9d9b3e1aedca2b4e74db3d5bf7
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2019-07-12 18:57:01 -0500

    Barra de búsqueda

---
+++ practicas/blog/buscar.php
@@ -0,0 +1,33 @@
+<?php 
+
+require 'admin/config.php';
+require 'functions.php';
+
+$conexion = conexion($bd_config);
+
+if(!$conexion){
+	header('Location: error.php');
+}
+
+if($_SERVER['REQUEST_METHOD']=='GET' && !empty($_GET['busqueda'])){
+	$busqueda = limpiarDatos($_GET['busqueda']);
+
+	$statement= $conexion->prepare(
+		'SELECT * FROM articulos WHERE titulo LIKE :busqueda or (texto LIKE :busqueda or extracto LIKE :busqueda)'
+	);
+
+	$statement->execute(array(':busqueda'=>"%$busqueda%"));
+	$resultados = $statement->fetchAll();
+
+	if(empty($resultados)){
+		$titulo= 'No se encontraron artículos con el resultado: '. $busqueda;
+	}else{
+		$titulo= 'Resultados de la busqueda: '. $busqueda;
+	}
+}else{
+	header('Location: '.RUTA.'/index.php');
+}
+require 'views/buscar.view.php'
+
+
+ ?>
